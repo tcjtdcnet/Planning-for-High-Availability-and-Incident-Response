51dd684257d368b53f6ad6181fae7fd58d50e4ef
tcjtdcnet <tcj@tdcnet.dk>
2025-03-10 12:11:31 +0100

    .

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
@@ -5,6 +5,6 @@
    account            = data.aws_caller_identity.current.account_id
    aws_ami            = "ami-0b9064170e32bde34"
    private_subnet_ids = module.vpc.private_subnet_ids
-   public_subnet_ids  = 
-   vpc_id             = 
+   public_subnet_ids  = module.vpc.public_subnet_ids 
+   vpc_id             = module.vpc.vpc_id
  }
