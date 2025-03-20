1c91ce61ec4e037d27abc8e3149dc0c0cc550fce
tcjtdcnet <tcj@tdcnet.dk>
2025-03-20 14:54:30 +0100

    Prepare for 5.12

--- lesson-5-rds-deploy/exercises/starter/zone2/_data.tf
+++ lesson-5-rds-deploy/exercises/starter/zone2/_data.tf
@@ -19,7 +19,7 @@ data "aws_caller_identity" "current" {}
 data "terraform_remote_state" "primary" {
   backend = "s3" 
   config = {
-     bucket = "udacity-sre-terraform-testing" # Replace it with your S3 bucket name which is in us-east-2 region
+     bucket = "udacity-tf-east2-tcj" # Replace it with your S3 bucket name which is in us-east-2 region
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
